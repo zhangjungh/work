bf28f840a686ccba295a46c36108dbdb2e96972a
zhangjungh <zhangjun.cn@gmail.com>
2014-02-14 22:22:17 +0800

    semss format; add run_test

--- python/rename_pdf.py
+++ python/rename_pdf.py
@@ -24,6 +24,7 @@ def main(argv):
 		(opts, args) = getopt.getopt(argv[1:], 'i:o:')
 	except getopt.GetoptError:
 		return usage()
+	if len(argv) == 1 and run_test(): return
 	if not opts: return usage()
 	
 	idir = None
@@ -31,19 +32,26 @@ def main(argv):
 	for (k, v) in opts:
 		if k == '-i': idir = v
 		elif k == '-o': odir = v
-	if os.path.exists(idir):
-		if not os.path.exists(odir):
-			print('create output dir: %s' % odir)
-			os.makedirs(odir)
-		for root, dirs, files in os.walk(idir):
-			for file in files:
-				path = os.path.join(root, file)
-				with open(path, 'rb') as f:
-					outfile = pdf_process(f)
-					print('copy from %s to %s' % (file, outfile))
-					shutil.copy(path, os.path.join(odir, outfile).decode('utf-8'))
+		
+	if idir and odir and os.path.exists(idir):
+		folder_process(idir, odir)
 
-def pdf_gettext(fp):
+def folder_process(idir, odir, reserve=False):
+	if not os.path.exists(odir):
+		print('create output dir: %s' % odir)
+		os.makedirs(odir)
+	for root, dirs, files in os.walk(idir):
+		for file in files:
+			if file.lower().endswith('.pdf'):
+				try:
+					path = os.path.join(root, file)
+					outfile = pdf_process(path, reserve)
+					print('copy from %s to %s' % (file, outfile))
+					shutil.copy(path, os.path.join(odir, outfile).decode('utf-8'))				
+				except Exception, e:
+					print('error: %s -> %s' % (file, str(e)))	
+
+def pdf_gettext(filepath, reserve):
 	# debug option
 	debug = 0
 	# input option
@@ -63,6 +71,16 @@ def pdf_gettext(fp):
 	lastout = 'lastout.html'
 	firstpage = None
 	lastpage = None
+	first = []
+	last = []
+	
+	if reserve:
+		firstout = filepath[:-3] + firstout
+		lastout = filepath[:-3] + lastout
+		if os.path.exists(firstout) and os.path.exists(lastout):					
+			html_textparser(firstout, first)
+			html_textparser(lastout, last)
+			return first, last
 	#
 	PDFDocument.debug = debug
 	PDFParser.debug = debug
@@ -91,27 +109,28 @@ def pdf_gettext(fp):
 	
 	interpreter = PDFPageInterpreter(rsrcmgr, device)
 	
-	for page in PDFPage.get_pages(fp, pagenos,
-								maxpages=maxpages, password=password,
-								caching=caching, check_extractable=True):
-		if not firstpage:
-			firstpage = page
-		else:
-			lastpage = page
-	
-	interpreter.process_page(firstpage)
-	with open(firstout, 'w') as f:
-		f.write(outfp.getvalue())
-	outfp.truncate(0)
-	interpreter.process_page(lastpage)
-	with open(lastout, 'w') as f:
-		f.write(outfp.getvalue())
+	with open(filepath, 'rb') as fp:
+		for page in PDFPage.get_pages(fp, pagenos,
+									maxpages=maxpages, password=password,
+									caching=caching, check_extractable=True):
+			if not firstpage:
+				firstpage = page
+			else:
+				lastpage = page
+		
+		if not lastpage:
+			raise Exception, 'only one page, disregarded'
+			
+		interpreter.process_page(firstpage)
+		with open(firstout, 'w') as f:
+			f.write(outfp.getvalue())
+		outfp.truncate(0)
+		interpreter.process_page(lastpage)
+		with open(lastout, 'w') as f:
+			f.write(outfp.getvalue())
 		
 	device.close()	
 	outfp.close()
-	
-	first = []
-	last = []
 	
 	html_textparser(firstout, first)
 	html_textparser(lastout, last)
@@ -154,12 +173,14 @@ def html_textparser(filename, list):
 						tcur += len(m[1])
 					list.append( (t, int(m[0]), left, int(d[1])) )
 
-def pdf_process(fp):
-	first, last = pdf_gettext(fp)	
+def pdf_process(filepath, reserve):
+	first, last = pdf_gettext(filepath, reserve)	
 	if is_NEJM(first):
 		return NEJM_process(first, last)
 	elif is_Spine(first):
 		return Spine_process(first)
+	elif is_semss(first):
+		return semss_process(first, last)
 	else:
 		global g_count
 		g_count += 1
@@ -174,21 +195,25 @@ def text_gettitle(list):
 			title = i[0]
 			break
 			
-	index = -1
-	for i in range(len(list)):
-		if index != -1:
-			if list[i][1] != list[index][1]:
-				break
-			title += ' ' + list[i][0]
-		if index == -1 and title == list[i][0]:
-			index = i
+	t = None
+	for i, p in enumerate(list):
+		if t != None:
+			if t[1] != p[1]: break
+			title += ' ' + p[0]
+		if not t and title == p[0]:
+			t = p
+	
+	#get valid title:
+	title = re.sub(r'[\/:*?"<>|]', '--', title)
+	
 	return title
 			
-def text_getnum(list):
-	l = sorted(list, key=lambda l : l[3], reverse=True)
+def text_getnum(list, footer=True):
+	r = -20 if footer else 20
+	l = sorted(list, key=lambda l : l[3], reverse=footer)
 	lc, top, num = [], -1, '0000'
 	for i in l:
-		if top != -1 and i[3] < top-20:
+		if top != -1 and i[3] < top+r:
 			break
 		if i[0].isdigit():
 			if top == -1: top = i[3]
@@ -199,15 +224,14 @@ def text_getnum(list):
 			edge = abs(i[2]-300)
 			num = i[0]			
 	return num
-		
+	
 def is_NEJM(list):
 	pattern = [[0, 'new england journal'], [0, 'new  england  journal'], [0, 'www.nejm.org'], [0, 'n engl j med']]
 	for p in list:
 		for i in pattern:
 			if p[0].lower().find(i[1]) != -1:
-				#print (p)
-				i[0] = 1
-	return sum([i[0] for i in pattern]) >= 1
+				return True
+	return False
 	
 def NEJM_process(first, last):
 	#get the title
@@ -219,7 +243,7 @@ def NEJM_process(first, last):
 	#get the last pagenum
 	end = text_getnum(last).zfill(3)
 	
-	return '%s-%s.%s.pdf'%(start, end[ len(end)-3:len(end) ], title)
+	return '%s-%s.%s.pdf'%(start, end[-3:], title)
 
 def is_Spine(list):
 	pattern = 'the spine journal'
@@ -241,7 +265,45 @@ def Spine_process(first):
 				start, end = mg[-2].zfill(4), mg[-1].zfill(3)
 			break
 	
-	return '%s-%s.%s.pdf'%(start, end[ len(end)-3:len(end) ], title)
+	return '%s-%s.%s.pdf'%(start, end[-3:], title)
+
+def is_semss(list):
+	pattern = [[0, 'seminars in spine surgery'], [0, 'j.semss']]
+	for p in list:
+		for i in pattern:
+			if p[0].lower().find(i[1]) != -1:
+				return True
+	return False
+	
+def semss_process(first, last):
+	title = text_gettitle(first)
+	
+	digit = re.compile(r'(\d+)')
+	start, end = '0000', '000'
+	pattern = 'S E M I N A R S I N S P I N E S U R G E R Y'
+	for p in first:
+		if p[0].find(pattern) != -1:
+			mg = digit.findall(p[0].translate(None, ' '))
+			if len(mg) >= 2:
+				start, end = mg[-2].zfill(4), mg[-1].zfill(3)
+			break
+	
+	if start == '0000':
+		start = text_getnum(first).zfill(4)	
+		end = text_getnum(last, False).zfill(3)
+	
+	return '%s-%s.%s.pdf'%(start, end[-3:], title)
+	
+def run_test():
+	path = r'C:\Users\jzhang\Downloads'
+	ret = False
+	if os.path.exists(path + r'\run_test'):
+		for i in '123':
+			p = path + r'\input' + i
+			if os.path.exists(p):
+				ret = True
+				folder_process(p, p+'_O', True)
+	return ret
 	
 if __name__ == '__main__':
 	main(sys.argv)
